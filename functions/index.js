712731332fde28c21330e98207bf6989f0f2d31a
levizwannah <levizwannah@gmail.com>
2022-10-31 19:11:04 +0300

    finished the template

---
+++ functions/index.js
@@ -0,0 +1,98 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const Mpesa = require("mpesa-node");
+
+admin.initializeApp();
+
+// config object
+const config = {
+    key: "consumer key",
+    secret: "consumer secret",
+    passkey: "till passkey",
+    shortcode: "short code",
+    till: "till"
+}
+
+const mpesaClient = new Mpesa({
+    consumerKey: config.key,
+    consumerSecret: config.secret,
+    enviroment: "production",
+    shortCode: config.shortcode,
+    lipaNaMpesaShortCode: config.till,
+    lipaNaMpesaShortPass: config.passkey,
+    //... and others as necessary.
+    //... check the documentation for mpesa-node library
+});
+
+// hit this url to register your urls
+exports.registerUrl = functions.https.onRequest((req, res) => {
+    mpesaClient.c2bRegister(`${url}/c2bSuccess`, `${url}/c2bValidate`)
+})
+
+// do your confirmation logic in here
+exports.c2bSuccess = functions.https.onRequest((req, res) => {
+
+    // this is not really necessary, but let's just respond
+    return res.json({
+        ResultCode: 0,
+        ResultDesc: "Accepted"
+    });
+
+    // do confirmation logic here
+    const type = req.TransactionType;
+    const transactionId = req.TransID;
+    const time = req.TransTime;
+    const amount = req.TransAmount;
+    const shortCode = req.BusinessShortCode;
+    const ref = req.BillRefNumber;
+    const invoiceNum = req.InvoiceNumber;
+    const balance = req.OrgAccountBalance;
+    const otherTransId = req.ThirdPartyTransID;
+    const phone = req.MSISDN;
+    const firstName = req.FirstName;
+    const middleName = req.MiddleName;
+    const lastName = req.LastName;
+
+    // do some work here
+
+});
+
+// do your validation logic in here
+exports.c2bValidate = functions.https.onRequest((req, res) => {
+  
+    const type = req.TransactionType;
+    const transactionId = req.TransID;
+    const time = req.TransTime;
+    const amount = req.TransAmount;
+    const shortCode = req.BusinessShortCode;
+    const ref = req.BillRefNumber;
+    const invoiceNum = req.InvoiceNumber;
+    const balance = req.OrgAccountBalance;
+    const otherTransId = req.ThirdPartyTransID;
+    const phone = req.MSISDN;
+    const firstName = req.FirstName;
+    const middleName = req.MiddleName;
+    const lastName = req.LastName;
+
+    let accepted = true;
+
+    // do something here to manipulate the `accepted` variable.
+    // code...
+    
+    // do validation logic here
+
+    if(!accepted){
+        return res.json({
+            ResultCode: "C2B00016", // look at daraja for the various codes
+            ResultDesc: "Rejected"
+        });
+    }
+
+    return res.json({
+        ResultCode: 0,
+        ResultDesc: "Accepted"
+    });
+});
+
+
+
